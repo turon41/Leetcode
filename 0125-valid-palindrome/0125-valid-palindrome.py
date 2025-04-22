1dbea28141f84e6fd561b694853d2d4049bf6a4d
turon41 <139960348+turon41@users.noreply.github.com>
2025-04-22 16:42:05 +0600

    Time: 2 ms (99.28%), Space: 18 MB (83.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,6 +3,6 @@ class Solution:
         s = s.lower()
         ans = ""
         for i in s:
-           if (ord(i)<=122 and ord(i)>=97) or (ord(i)<=57 and ord(i)>=48) : ans+=i
+           if i.isalnum(): ans+=i
         
         return ans==ans[::-1]
