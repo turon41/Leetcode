6e6429e6e5bacd5b49794c315947e871fa12441c
turon41 <139960348+turon41@users.noreply.github.com>
2025-05-09 09:41:12 +0600

    Time: 0 ms (100%), Space: 17.6 MB (83.41%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,15 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        h = defaultdict(int)
-        for num in nums:
-            h[num]+=1
-        ind = 0    
-        for i in range(3):
-            while h[i]>0:
-                nums[ind]=i
-                ind+=1
-                h[i]-=1
+        "Dutch National Flag Algorithm"
+        
+        n = len(nums)
+        l,mid,h = 0,0,n-1
+        while mid<=h:
+            if nums[mid]==0:
+                nums[l],nums[mid] = nums[mid],nums[l]
+                l+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[h] = nums[h],nums[mid]
+                h-=1
+
 
 
             
