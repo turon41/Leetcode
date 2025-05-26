deb3de2c272189e36ed7f4fbe4014dcf4c20a7ec
turon41 <139960348+turon41@users.noreply.github.com>
2025-05-26 11:39:24 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -5,7 +5,7 @@ class Solution:
 
         for a in asteroids:
 
-            while res and a < 0 < res[-1]:
+            if res and a < 0 < res[-1]:
                 if -a>res[-1]:
                     res.pop()
                     continue
