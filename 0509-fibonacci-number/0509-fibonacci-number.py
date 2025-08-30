4572081bcee2196864fd69d8d51ca882881356be
turon41 <139960348+turon41@users.noreply.github.com>
2025-08-30 17:51:47 +0600

    Time: 29 ms (95.77%), Space: 17.5 MB (98.79%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,6 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def fib(self, n: int) -> int:
-        #USING DP
+        #USING DP BOTTOM UP APPROACH OR TABULATION METHOD
         if n==0:
             return 0
         dp = [-1]*(n+1)
