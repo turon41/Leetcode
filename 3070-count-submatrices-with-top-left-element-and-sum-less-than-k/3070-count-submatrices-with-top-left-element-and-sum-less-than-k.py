6252dcd3043375cb673eb3c9eb77d0837f0db972
turon41 <139960348+turon41@users.noreply.github.com>
2026-03-19 02:36:37 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -1,14 +1,7 @@
 # Added using AI
 class Solution:
     def countSubmatrices(self, grid: list[list[int]], k: int) -> int:
-        m, n = len(grid), len(grid[0])
-        ans = 0
-        px = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m):
-            for j in range(n):
-                px[i+1][j+1] = grid[i][j] + px[i][j+1] + px[i+1][j] - px[i][j]
-                if px[i+1][j+1] <= k:
-                    ans += 1
-                else:
-                    break    
-        return ans
+        grid = map(accumulate,grid)
+        grid = map(accumulate,zip(*grid))
+
+        return sum(val<=k for row in grid for val in row)
