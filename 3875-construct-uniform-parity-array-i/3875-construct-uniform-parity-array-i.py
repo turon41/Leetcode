86d96327ca6d5c6869831c7b7a86374aa8d15efb
turon41 <139960348+turon41@users.noreply.github.com>
2026-03-24 11:25:25 +0600

    Time: 0 ms (100%), Space: 19.4 MB (20.65%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        # cnt1 = 0
+        # cnt2 = 0
+        # for num in nums1:
+        #     if num&1:
+        #         cnt1+=1
+        #     else:
+        #         cnt2+=1
+        return True 
+        
